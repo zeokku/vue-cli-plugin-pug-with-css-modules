61501c5add45aba616a1333eb0b0cd54d26cc277
Luty <r89534217610@gmail.com>
2021-02-08 18:34:34 +0300

    some clean up

--- nameGeneratorContext.js
+++ nameGeneratorContext.js
@@ -5,7 +5,8 @@ const path = require('path');
 const nameGeneratorContext = () => {
     let namesMap = {};
 
-    let nameGenerator = incstr.idGenerator({ alphabet: '-_abcdefghijklmnopqrstuvwxyz0123456789' })
+    //move dash to the end to optimize name generation
+    let nameGenerator = incstr.idGenerator({ alphabet: '_abcdefghijklmnopqrstuvwxyz0123456789-' })
 
     //the function is called for each CSS rule, so cache the pairs of minified name with og name
     return (context, localIdentName, localName, options) => {
@@ -14,14 +15,13 @@ const nameGeneratorContext = () => {
 
         if (namesMap[key]) return (namesMap[key]);
 
-        let name = undefined;
+        let name = nameGenerator();
 
-        do {
-            name = nameGenerator()
-        }
-        while (/^[-\d]|(?:[-_]+|^)ad/.test(name));
         //hypen prefixes are reserved for vendor classes, also it can't start with a digit
         //in addition exclude ^ad or any _ad, -ad constructions to avoid adblock problem
+        while (/^[-\d]|(?:[-_]+|^)ad/.test(name)) {
+            name = nameGenerator();
+        }
 
         namesMap[key] = name;
 
